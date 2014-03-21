00856f5e17d44c09fdf16d9019768a98cc262b31
Josh Leeman <joshleeman@gmail.com>
2014-03-20 21:29:11 -0400

    upload calc.rb

---
+++ calc.rb
@@ -0,0 +1,12 @@
+# calculate hours in a year
+puts 'hours in a year'
+puts 365 * 24
+# calculate minutes in a decade
+puts 'minutes in a decade'
+puts ((24*60) * 365) * 10
+# calculate age in seconds
+puts 'my age in seconds'
+puts (((60 * 60) * 24) * 365) * 39
+# author age
+puts 'author age'
+puts 1160000000/(60*60*24*365)
