14f9f07474abea5f6adfc2caa61cd98405fbecd9
Josh Leeman <joshleeman@gmail.com>
2014-03-20 21:30:21 -0400

    chapter5_6_full_name.rb

---
+++ chapter5_6_full_name.rb
@@ -0,0 +1,8 @@
+puts 'What\'s your first name?'
+first_name = gets.chomp
+puts 'What\'s your middle name?'
+middle_name = gets.chomp
+puts 'What\'s your last name?'
+last_name = gets.chomp
+
+puts "Your name is #{first_name} #{middle_name} #{last_name}!"
