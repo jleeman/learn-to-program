a5b25a9b3157f6b3df30bad3e5492f816f163531
josh <josh@Joshua-Leeman.local>
2014-03-21 17:15:05 -0400

    commit chapter6_2_toc.rb

---
+++ chapter6_2_toc.rb
@@ -0,0 +1,5 @@
+width = 80
+puts ('Table of Contents'.center(width))
+puts ('Chapter 1: Getting Started'.ljust(width/2) + 'page 1'.rjust(width/2))
+puts ('Chapter 2: Numbers'.ljust(width/2) + 'page 9'.rjust(width/2))
+puts ('Chapter 3: Letters'.ljust(width/2) + 'page 13'.rjust(width/2))
